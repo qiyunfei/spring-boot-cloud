f4eb1972a0884511a7460db30e49152a402a3523
yunfei.qi <yunfei.qi@mljr.com>
2017-08-13 00:32:32 +0800

    add new code

---
+++ springcloud-learn/src/main/java/com/f1ne/springcloud/learn/SpringcloudLearnApplication.java
@@ -0,0 +1,12 @@
+package com.f1ne.springcloud.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudLearnApplication.class, args);
+	}
+}
