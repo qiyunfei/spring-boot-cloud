b1c00ebf0b75f75a39a44b96e3bcd72573ec70eb
yunfei.qi <yunfei.qi@mljr.com>
2017-08-13 01:20:20 +0800

    add new code

---
+++ euraka-service/src/main/java/com/f1ne/springcloud/eureka/service/EurakaServiceApplication.java
@@ -0,0 +1,12 @@
+package com.f1ne.springcloud.eureka.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurakaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurakaServiceApplication.class, args);
+	}
+}
