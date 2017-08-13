c5c4a0370ed11de9a874441c1037700fdfe8027e
yunfei.qi <yunfei.qi@mljr.com>
2017-08-13 12:58:55 +0800

    add new code

--- springcloud-learn/src/main/java/com/f1ne/springcloud/learn/controller/HelloSpringCloudController.java
+++ springcloud-learn/src/main/java/com/f1ne/springcloud/learn/controller/HelloSpringCloudController.java
@@ -1,12 +1,24 @@
 package com.f1ne.springcloud.learn.controller;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloSpringCloudController {
-    @RequestMapping("/hello")
+    private final Logger logger = Logger.getLogger(getClass());
+    
+    @Autowired
+    private DiscoveryClient client ;
+    
+    @RequestMapping(value="/hello" ,method=RequestMethod.GET)
     public String hello(){
+        ServiceInstance instance = client.getLocalServiceInstance();
+        logger.info("hello , host:"+instance.getHost()+" , service_id:"+instance.getServiceId());
         return "hello world spring cloud!";
     }
 
