55a2d7d435f4a4c292fe8efb32c3077a4693209d
qiyunfei <cust_qiyunfei@163.com>
2017-09-27 21:21:36 +0800

    add code

---
+++ feign-client/src/main/java/com/f1ne/springcloud/feign/learn/FeignClientApplication.java
@@ -0,0 +1,12 @@
+package com.f1ne.springcloud.feign.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignClientApplication.class, args);
+	}
+}
